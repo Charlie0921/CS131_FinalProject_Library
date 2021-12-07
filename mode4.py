23a15bd5fbbdc10b607ed81856e111744bba9da3
dzk5572 <dzk5572@gmail.com>
2021-12-06 23:04:30 -0500

    Update mode 3, 4

---
+++ mode4.py
@@ -0,0 +1,12 @@
+# library4
+# ratio 순으로 sort (selection / merge ...)
+
+def sortlist(ratio_comparison):
+    order = len(ratio_comparison)
+    for i in range(0, order):
+        for j in range(0, order-i-1):
+            if(ratio_comparison[j][1] > ratio_comparison[j+1]):
+                temp = ratio_comparison[j]
+                ratio_comparison[j] = ratio_comparison[j+1]
+                ratio_comparison[j+1] = temp
+    return ratio_comparison
