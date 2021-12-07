7a31061d2c0e47bb8b750dceeeaf35de4492f377
dzk5572 <dzk5572@gmail.com>
2021-12-07 00:13:25 -0500

    Co-authored-by: Yoojun Kim <yjk82693@users.noreply.github.com> Co-authored-by: Charlie0921 <Charlie0921@users.noreply.github.com> Co-authored-by: jonghyun0825 <jonghyun0825@users.noreply.github.com>

--- mode3.py
+++ mode3.py
@@ -1,12 +1,5 @@
 '''
 
-1. 분자 - cache_book.txt 데이터 사용
-a) 데이터에서 line 추출
-b) Borrow Time을 list 형태로 변환(end of the log까지 반납 x -> max = log end date)
-c) 데이터 속에서 max - min = length(size)
-d) sum of length(size)가 책 하나의 usage ratio가 됨
-e) 책별로 계속 반복
-
 2. 분모 - cache_book.txt 데이터 사용
 a) 데이터에서 line을 추출
 b) Total Copies의 리스트들만 추출
@@ -31,37 +24,33 @@ def ratiotest(book_ratio):
         copies.append(t[1])
 
     # booklist에서 가져온 것들
-
-    duration = []
-    # duration = 빌린 기간
-    for i in range(len(booklist)):
-        d = list(booklist[i])
-        duration.append(d[4])
-
-    data = []
     # changed amount of book
+    # 분모
+
+    Booknote = list(cache_book)
+    Tcopy = list(cache_book[1])
+    Ttotal = list(Tcopy[:-1])
+    Total = list(Ttotal[1])
+    copies.append(Total)
+
+    # total amount of rent
+    # 분자
     '''
-    #1) when more books are supplied
-    if()
-    copies[i] += 1
-    p=[4,16]
-    min = p[0]
-    max = p[1]
-    range = max-min+1
-    print(range)
+    1. 분자 - cache_book.txt 데이터 사용
+    a) 데이터에서 line 추출
+    b) Borrow Time을 list 형태로 변환(end of the log까지 반납 x -> max = log end date)
+    c) 데이터 속에서 max - min = length(size)
+    d) sum of length(size)가 책 하나의 usage ratio가 됨
+    e) 책별로 계속 반복
     '''
-    # 2) when books are rented
-    k = 0
-    for p in range(booklist):
-        rent = list(booklist[p])
-        if(rent[p] == Book):
-            k += 1
-        x = copies[i] - k
-        data.append(x)
+    Rent = list(cache_book[])
+
+    # Calcularing Rate
     rate = []
     for j in range(len(copies)):
         r = copies[j]-data[j]/copies[j]
         r.append(rate)
+    # Creating 2D List (Book name / Rates)
     bookratio = []
     for k in range(len(book)):
         x = []
